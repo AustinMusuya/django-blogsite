3f29fe7dc91c971de0e99f15e5cec9beb8aefd8b
AustinMusuya <amusuya57@gmail.com>
2024-11-17 15:31:30 +0300

    create url path for create-post and import create_post view from views

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('sign-up/', views.sign_up, name='sign-up'),
     path('logout/', views.user_logout, name='user_logout'),
+    path('create-post/', views.create_post, name='create-post'),
 ]
